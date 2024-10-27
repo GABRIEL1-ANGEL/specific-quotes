2ab88e9ae93e35ad735a4711d382052345d5e0c3
Onwuka ekene <onwukadavid732@gmail.com>
2024-10-27 14:42:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+$("button.a").click(function() {
+    $("h1").text('We cannot solve problems with the kind of thinking we employed when we came up with them.');
+});
+
+$("button.b").click(function() {
+    $("h1").text('It is better to fail in originality than to succeed in imitation.');
+});
+
+$("button.c").click(function(){
+    $("h1").text( 'Success is not final; failure is not fatal: It is the courage to continue that counts.');
+})
+
+$("button.d").click(function(){
+    $("h1").text( 'The road to success and the road to failure are almost exactly the same.');
+})
+
+$("button.e").click(function(){
+    $("h1").text('Success usually comes to those who are too busy to be looking for it');
+})
